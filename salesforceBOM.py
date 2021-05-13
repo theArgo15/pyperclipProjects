0b4c781c254f27d16325e6a9507a5dbc2705b3b6
theArgo15 <70275138+theArgo15@users.noreply.github.com>
2021-05-13 13:21:29 -0500

    consolidate print statements

--- salesforceBOM.py
+++ salesforceBOM.py
@@ -1,13 +1,13 @@
-import re
 import pyperclip
 
 # data is taken directly from clipboard. Copy an excel column with cells that have information subdivided by semicolon.
 data=str(pyperclip.paste())
-print(f'scanning {len(data)} objects')
+print(f'Scanning {len(data)} objects')
 # these methods create a list from data that is subdivided by both new row and by semicolon
 cleanData = ';'.join(data.split()).split(';')
 cleanData.sort()
 # outputs data as a list separated by newlines. This can be pasted into an email or spreadsheet
 resultString = '\n'.join(cleanData)
-print(resultString)
+print(f'The BOM has {len(cleanData)} items.')
 pyperclip.copy(str(resultString))
+print('The BOM is now in your clipboard')
